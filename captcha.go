fbe45eb7b3a3ecc6d08d4bccfa55a5a97b5cb98c
anubizkit <thanawat.pengchaimo@ktb.co.th>
2018-12-17 16:44:22 +0700

    reflect right

--- captcha.go
+++ captcha.go
@@ -1,13 +1,25 @@
 package captcha
 
 import (
-	
+
 )
 
 func Captcha(pettern int, left int, oper int, right int) string {
-	if right == 2 {
-		return "1 + two"
-	} else {
-		return "1 + one"
+	return "1 + "+IntToStr(right)
+}
+
+func IntToStr(num int) string {
+	switch num {
+		case 0 : return "zero"
+		case 1 : return "one"
+		case 2 : return "two"
+		case 3 : return "three"
+		case 4 : return "four"
+		case 5 : return "five"
+		case 6 : return "six"
+		case 7 : return "seven"
+		case 8 : return "eight"
+		case 9 : return "nine"
 	}
+	return ""
 }
